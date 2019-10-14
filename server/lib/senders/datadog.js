5f7d04cb16738a353c3a72176b7cef9783bc43cf
Ali Ince <ali.ince@codart.co.uk>
2019-10-14 17:22:28 +0100

    Latest extension updates

--- server/lib/senders/datadog.js
+++ server/lib/senders/datadog.js
@@ -7,36 +7,54 @@ const logger = require('../logger');
 module.exports = () => {
   const ddMaxBatch = 200
 
-  const ddUrl = config('DATADOG_URL');
+  const ddBaseUrl = config('DATADOG_URL');
   const ddApiKey = config('DATADOG_API_KEY')
   const ddLogTags = config('DATADOG_LOG_TAGS') || ''
   const ddLogService = config('DATADOG_LOG_SERVICE') || ''
   const ddLogSource = config('DATADOG_LOG_SOURCE') || ''
-  const ddUrl = `${ddUrl}/${ddApiKey}?ddtags=${ddLogTags}&ddsource=${ddLogSource}&service=${ddLogService}`
+  const ddUrl = `${ddBaseUrl}/${ddApiKey}`
 
-  const ddConcurrentCalls = parseInt(config('DATADOG_CONCURRENT_CALLS'), 10) || 5;
+  const concurrentCalls = parseInt(config('DATADOG_CONCURRENT_CALLS'), 10) || 5;
   const batchMode = config('SEND_AS_BATCH') === true || config('SEND_AS_BATCH') === 'true';
 
-  const sendRequest = (data, callback) =>
+  const enrichItem = (item) => {
+    if (ddLogService) {
+      item.service = ddLogService
+    }
+    if (ddLogSource) {
+      item.ddsource = ddLogSource
+    }
+    if (ddLogTags) {
+      item.ddtags = ddLogTags
+    }
+  }
+
+  const sendRequest = (data, callback) => {
+    logger.debug(`sending data to '${ddUrl}'`);
+    logger.debug(`data is '${JSON.stringify(data)}'`);
+
     Request({
       method: 'POST',
       url: ddUrl,
       json: true,
       body: data
     }, (err, response, body) => {
+      logger.debug(`result => statusCode: ${response.statusCode}, err: ${err}, body: ${body}`);
+
       if (err || response.statusCode < 200 || response.statusCode >= 400) {
         return callback(err || body || response.statusCode);
       }
 
       return callback();
     });
+  };
 
-    const chunk = (arr, chunkSize) => {
-      const chunks = [];
-      for (var i=0,len=arr.length; i<len; i+=chunkSize)
-        chunks.push(arr.slice(i,i+chunkSize));
-      return chunks;
-    };
+  const chunk = (arr, chunkSize) => {
+    const chunks = [];
+    for (var i = 0, len = arr.length; i < len; i += chunkSize)
+      chunks.push(arr.slice(i, i + chunkSize));
+    return chunks;
+  };
 
   return (logs, callback) => {
     if (!logs || !logs.length) {
@@ -45,12 +63,16 @@ module.exports = () => {
 
     logger.info(`${logs.length} logs found.`);
 
+    logger.debug(`enriching items with datadog entries.`);
+    logs.forEach(enrichItem);
+    logger.debug(`enrich complete => ${logs}`);
+
     let items = logs;
     if (batchMode) {
       items = chunk(logs, ddMaxBatch);
     }
 
-    logger.info(`Sending to '${ddUrl}' with ${concurrentCalls} concurrent calls.`);
+    logger.info(`Sending ${items.length} items to '${ddBaseUrl}' using ${concurrentCalls} concurrent calls.`);
     return async.eachLimit(items, concurrentCalls, sendRequest, callback);
   };
 };
